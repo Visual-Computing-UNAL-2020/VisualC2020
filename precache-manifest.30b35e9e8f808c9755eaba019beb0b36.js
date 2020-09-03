7f55ae690c08a289c81c086a5afb0162bb0311cf
Ivan Solano <idsolanov@unal.edu.co>
2020-09-03 09:50:33 -0500

    Updates

---
+++ precache-manifest.30b35e9e8f808c9755eaba019beb0b36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35cf3d08bdf42dcde48b35bc147784a3",
+    "url": "/VisualC2020/index.html"
+  },
+  {
+    "revision": "b36367c780aed67e8edd",
+    "url": "/VisualC2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b383a325ccecaeaba3a3",
+    "url": "/VisualC2020/static/js/2.e47d8a23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VisualC2020/static/js/2.e47d8a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b36367c780aed67e8edd",
+    "url": "/VisualC2020/static/js/main.3786322d.chunk.js"
+  },
+  {
+    "revision": "3d2a80add4661be4e1a8",
+    "url": "/VisualC2020/static/js/runtime-main.37603b73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VisualC2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
