9d23df80580a5b261ec9a6558756de9988e68814
Ivan Solano <idsolanov@unal.edu.co>
2020-09-03 10:52:44 -0500

    cambio de estructura

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './Components/menu'
+import './Css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Computacion Visual UNAL 2020 -2</h1>
+        <Menu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class menu extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <h1>menu</h1>
+            </div>
+        )
+    }
+}
+
+export default menu
