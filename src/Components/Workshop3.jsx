c045ce7fb1eae88b8b6b2256edfaa757274ac799
mtvelasquezg <mtvelasquez@htsoft.co>
2020-11-26 10:14:11 -0500

    se agregan luces combinadas

--- src/Components/Workshop3.jsx
+++ src/Components/Workshop3.jsx
@@ -21,6 +21,7 @@ import HeringIllusion from "./p5/Workshop2-Canvas/HeringIllusion"
 import AmbientLight from "./three/AmbientLight";
 import LightAttenuation from "./three/LightAttenuation";
 import Fog from "./three/Fog";
+import CombineLight from "./three/CombineLight";
 
 class Workshop3 extends React.Component{
 
@@ -35,7 +36,7 @@ class Workshop3 extends React.Component{
                         <h4>Ambient light</h4>
 
                         <p>Con la iluminacion AmbientLight las formas son planas ya que se multiplica el color del material por la color por la intensidad, esto no tiene dirección. La iluminacioón ambiental no es tan buena porque en realidad solo cambia el color de todo en la escena.</p>
-                        <p> En este ejemplo, se agregó una luz de ambiente más una luz direccional</p>
+                        <p> En este ejemplo, se agregó una luz de ambiente</p>
                         <Card className="jumbotron bg-dark px-0 py-0" id="canvasCard">
                             <AmbientLight/>
                         </Card>
@@ -57,6 +58,14 @@ class Workshop3 extends React.Component{
                             <Fog/>
                         </Card>
                         
+						
+						<h4>Combine lighting</h4>
+
+                        <p>Una propiedad básica de la luz es que pierde su intensidad cuanto más se aleja de su fuente. En el mundo físico, la atenuación es proporcional a 1 / d2 , donde des la distancia entre la fuente de luz y un objeto. El uso de la función 1 / d 2 hace que la luz disminuya muy rápidamente, por lo que es común hacer que la atenuación sea proporcional a 1 / d. </p>
+                        <p> En este ejemplo, se agregó una luz de ambiente más una luz direccional</p>
+                        <Card className="jumbotron bg-dark px-0 py-0" id="canvasCard">
+                            <CombineLight/>
+                        </Card>
 
 
                     </Card.Body>
--- src/Components/three/CombineLight.js
+++ src/Components/three/CombineLight.js
@@ -61,7 +61,18 @@ const AmbientLight = () => {
         {/* Luz ambiental */}
         <ambientLight intensity={0.3} />
         {/* Luz direccional desde arriba */}
-
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
         <group>
           {/* Este mesh es el piso */}
           <mesh
