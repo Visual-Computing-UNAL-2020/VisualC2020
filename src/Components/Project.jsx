8ac948ce200800db219cf7406393e7c32607874b
mtvelasquezg <mtvelasquez@htsoft.co>
2020-12-09 19:07:07 -0500

    se define motivacion de la profundizacion

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,21 +1,45 @@
 import React from 'react'
 
+//images
+import covid from '../Assets/profundizacion/covid19.jpg'
 
 //bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container,Row,Col,Card} from 'react-bootstrap'
 
 //css
+import '../styles/Project.css'
 
-
-class Project extends React.Component{
+class home extends React.Component{
     render(){
         return(
-            <div>
-                El proyecto
-            </div>
+            <Container id="fullpage" >
+                <Card>
+                    <Card.Body>
+                        <Row >
+                            <h1 >Profundizacion en Visualización de datos</h1>
+                        </Row>
+                        <Row >
+                            <h2>Motivación</h2>
+                        </Row>
+                        <Row >
+                            <Col className="col-sm-4">
+                                <img src={covid} id="covid" className="img-fluid" alt="Responsive image"/>
+                            </Col>
+                            <Col className="col-sm-8">
+                                <p>Nuestro grupo decidimos hacer la profundización en el tema de visualización de datos y el tema que elegimos fue covid 19 en la ciudad de Bogotá.</p>
+                                <p>Originalmente queríamos indagar sobre la deserción escolar en Bogotá pero el dataset que encontramos tenía muy poca información así que decidimos elegir un dataset más interesante y que tuviera más historias por contar y nos decidimos por el de covid 19.</p>
+                                <blockquote class="blockquote">
+                                    <footer class="blockquote-footer"><cite title="Source Title">https://datosabiertos.bogota.gov.co</cite></footer>
+                                </blockquote>
+                            </Col>
+                            
+                        </Row>
+                    </Card.Body>
+                </Card>
+                                              
+            </Container>
         )
     }
 }
-
-export default Project
+export default home
