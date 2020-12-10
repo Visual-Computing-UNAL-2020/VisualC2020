c78baf80955b391622d03f0c367a666abb380ca1
mtvelasquezg <mtvelasquez@htsoft.co>
2020-12-09 22:16:04 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/VisualC2020/precache-manifest.63e49164277ad5be55c109627da09879.js"
+  "/VisualC2020/precache-manifest.b78221d8e3abcfd3640777776f8919bf.js"
 );
 
 self.addEventListener('message', (event) => {
